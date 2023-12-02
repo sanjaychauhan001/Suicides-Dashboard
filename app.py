348ded70f1c35456f74a9dc6b6169e25d4c05d4a
sanjay <sanjayjustforyou123@gmail.com>
2023-12-02 19:12:03 +0530

    added graph using plotly

--- app.py
+++ app.py
@@ -1,116 +1,60 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
+import plotly.express as px
+
 df = pd.read_csv("suicide_data.csv")
 df['age'] = df['age'].str.split(" ").str[0]
-st.set_page_config(layout='wide',page_title='Suicide Analysis')
+st.set_page_config(layout='wide', page_title='Suicide Analysis')
 
 def Overall_suicides():
 
-    col1,col2 = st.columns(2)
-
     suicide_per_year = df.groupby("year")['suicides/100k pop'].mean().reset_index()
+    fig = px.line(suicide_per_year, x='year', y='suicides/100k pop',
+                  title="Worldwide Suicides by year", markers=True)
+    fig.add_hline(y=df['suicides/100k pop'].mean(), line_dash='dot', line_color="green"
+                   , annotation_text="mean")
+    st.plotly_chart(fig)
 
-    with col1:
-        st.subheader('suicides by year')
-        fig,ax = plt.subplots()
-        ax.plot(suicide_per_year['year'], suicide_per_year['suicides/100k pop'])
-        st.pyplot(fig)
+    suicides_by_gender = df.groupby('sex')['suicides/100k pop'].sum().reset_index()
+    fig2 = px.pie(suicides_by_gender, names='sex', values='suicides/100k pop',
+                  title="suicides by Gender")
+    st.plotly_chart(fig2)
 
-    suicides_by_gender = df.groupby('sex')['suicides/100k pop'].sum()
+    suicide_by_age = df.groupby('age')['suicides/100k pop'].sum().reset_index()
+    fig3 = px.pie(suicide_by_age, names='age', values='suicides/100k pop',
+                  title="suicides by Age")
+    st.plotly_chart(fig3)
 
-    with col2:
-        st.subheader("suicides by Gender")
-        fig2,ax2 = plt.subplots()
-        ax2.pie(suicides_by_gender.values, labels=suicides_by_gender.index, autopct='%1.1f%%')
-        st.pyplot(fig2)
-
-    col3 ,col4 = st.columns(2)
-
-    suicide_by_age = df.groupby('age')['suicides/100k pop'].sum()
-    with col3:
-        st.subheader("suicides by Age")
-        fig3,ax3 = plt.subplots()
-        ax3.pie(suicide_by_age.values, labels=suicide_by_age.index, autopct='%1.1f%%')
-        st.pyplot(fig3)
-
-    with col4:
-        st.subheader("Top 7 countries by scicides")
-        top7 = df.groupby('country')['suicides/100k pop'].mean().sort_values(ascending=False).head(7).reset_index()
-        fig4, ax4 = plt.subplots()
-        sns.barplot(data=top7, x="country",y='suicides/100k pop',ax=ax4)
-        st.pyplot(fig4)
+    top7 = df.groupby('country')['suicides/100k pop'].mean().sort_values(ascending=False).head(7).reset_index()
+    fig4 = px.bar(top7, x='country', y='suicides/100k pop', title="Top 7 countries by suicides")
+    st.plotly_chart(fig4)
 
     st.subheader("mean suicide")
     st.metric("suicides/100k (1985-2016)", df['suicides/100k pop'].mean())
 
 def load_suicides_by_gender():
-    col5,col6 = st.columns(2)
     male_suicides = df[df['sex'] == 'male'].groupby('year')['suicides/100k pop'].mean().reset_index()
     female_suicides = df[df['sex'] == 'female'].groupby('year')['suicides/100k pop'].mean().reset_index()
+    temp_df = male_suicides.merge(female_suicides, on='year')
+    temp_df.rename(columns={"suicides/100k pop_x": 'Male',
+                           'suicides/100k pop_y': 'Female'}, inplace=True)
 
-    with col5:
-        st.subheader("for male")
-        fig5,ax5 = plt.subplots()
-        ax5.plot(male_suicides['year'],male_suicides['suicides/100k pop'])
-        st.pyplot(fig5)
-
-    with col6:
-        st.subheader("for female")
-        fig6,ax6 = plt.subplots()
-        ax6.plot(female_suicides['year'],female_suicides['suicides/100k pop'])
-        st.pyplot(fig6)
-
+    fig5 = px.line(temp_df, x='year', y=temp_df.columns, markers=True,
+                   title='Worldwide suicide by Gender')
+    fig5.add_hline(y=df['suicides/100k pop'].mean(), line_dash='dot', line_color="green"
+                   ,annotation_text="mean")
+    st.plotly_chart(fig5)
 def load_suicides_by_age():
-
-    age5_14 = df[df['age'] == '5-14'].groupby('year')['suicides/100k pop'].mean().reset_index()
-    age15_24 = df[df['age'] == '15-24'].groupby('year')['suicides/100k pop'].mean().reset_index()
-    age25_34 = df[df['age'] == '25-34'].groupby('year')['suicides/100k pop'].mean().reset_index()
-    age35_54 = df[df['age'] == '35-54'].groupby('year')['suicides/100k pop'].mean().reset_index()
-    age55_74 = df[df['age'] == '55-74'].groupby('year')['suicides/100k pop'].mean().reset_index()
-    age75 = df[df['age'] == '75+'].groupby('year')['suicides/100k pop'].mean().reset_index()
-    col7,col8 = st.columns(2)
-    with col7:
-        st.subheader("for age 5-14")
-        fig7,ax7 = plt.subplots()
-        ax7.plot(age5_14['year'],age5_14['suicides/100k pop'])
-        st.pyplot(fig7)
-
-    with col8:
-        st.subheader("for age 15-24")
-        fig8,ax8 = plt.subplots()
-        ax8.plot(age15_24['year'],age15_24['suicides/100k pop'])
-        st.pyplot(fig8)
-
-    col9,col10 = st.columns(2)
-    with col9:
-        st.subheader("for age 25-34")
-        fig9,ax9 = plt.subplots()
-        ax9.plot(age25_34['year'],age25_34['suicides/100k pop'])
-        st.pyplot(fig9)
-
-    with col10:
-        st.subheader("for age 35-54")
-        fig10,ax10 = plt.subplots()
-        ax10.plot(age35_54['year'],age35_54['suicides/100k pop'])
-        st.pyplot(fig10)
-
-    col11,col12 = st.columns(2)
-    with col11:
-        st.subheader("for age 55-74")
-        fig11,ax11 = plt.subplots()
-        ax11.plot(age55_74['year'],age55_74['suicides/100k pop'])
-        st.pyplot(fig10)
-    with col12:
-        st.subheader("for age over 75")
-        fig12,ax12 = plt.subplots()
-        ax12.plot(age75['year'],age75['suicides/100k pop'])
-        st.pyplot(fig12)
+    temp_df1 = pd.pivot_table(df,index='year', columns='age', values='suicides/100k pop')
+    fig6 = px.line(temp_df1, x=temp_df1.index, y=temp_df1.columns, markers=True,
+                   title="Worldwide suicides by age")
+    fig6.add_hline(y=df['suicides/100k pop'].mean(), line_dash='dot', line_color="green"
+                   ,annotation_text="mean")
+    st.plotly_chart(fig6)
 
 st.sidebar.title("Suicide Dashboard")
 
-selected_option = st.sidebar.selectbox("select One",['Overall suicides', "Suicides by gender",'Suicides by age'])
+selected_option = st.sidebar.selectbox("select One", ['Overall suicides', "Suicides by gender",'Suicides by age'])
 if (selected_option == 'Overall suicides'):
     Overall_suicides()
 
